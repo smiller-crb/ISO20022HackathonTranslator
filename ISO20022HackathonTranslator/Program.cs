d7f1a38bb5822d9cb069231e29e0d3b53c759590
Stephan Miller <smiller@crossriverbank.com>
2021-03-16 12:13:36 -0500

    Add project files.

---
+++ ISO20022HackathonTranslator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISO20022HackathonTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
