eb872602be786dd483201fe37d2b14774ce64c2a
Stephan Miller <smiller@crossriverbank.com>
2021-03-16 12:19:58 -0500

    basic project files

---
+++ ISO20022HackathonTranslator/Mapping/PayementMessageMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISO20022HackathonTranslator.Mapping
+{
+    public static class PayementMessageMapper
+    {
+    }
+}
